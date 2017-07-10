7aa836bd698c387b3175157be3247418f01a46e8
Dion <dionvanderlinden@live.nl>
2017-07-10 20:28:54 +0200

    v3

--- GTA5MenuVC++/Menu.h
+++ GTA5MenuVC++/Menu.h
@@ -1,31 +1,50 @@
 #pragma once
+#include "..\inc\natives.h"
+#include "..\inc\types.h"
+#include "..\inc\enums.h"
+#include "..\inc\main.h"
+
 #include <string>
+#include <array>
 
 using std::string;
+
 struct RGBA {
 	int r, g, b, a;
 };
 
 #define MAIN_MENU_NAME "main_menu"
-class Menu {
+#define MAX_MENU_COUNT 100
+#define MAX_OPTION_COUNT 100
+
+class Menu
+{
 public:
-	static string GetCurrentMenu();
-	static void _ChangeMenu(const string& menuname);
-	static void MenuBack();
+	Menu();
+	Menu(const RGBA& titleTextColour, const RGBA& titleRectColour, const RGBA& selectionColour, const RGBA& optionColour, const RGBA& optionsRectColour);
 
-	static void Title(const string& title);
-	static bool Option(const string& option);
-	static bool MenuOption(const string& option, const string& menu);
-	static bool IntOption(const string& option, int *var, int min, int max, int stepSize = 1);
-	static bool FloatOption(const string& option, float *var, float min, float max, float stepSize = 0.1f);
-	static bool BoolOption(const string& option, bool *var);
+	string GetCurrentMenu() const;
 
-	static void DrawMenu();
-	static void UpdateInput();
+	void _ChangeMenu(const string& menuname);
+	void MenuBack();
 
-	static void SetColours(RGBA titleTextClr, RGBA titleRectClr, RGBA selectionClr, RGBA optionClr, RGBA optionsRectClr);
+	void Title(const string& title, const eFont font = FontPricedown);
+	bool Option(const string& option);
+	bool MenuOption(const string& option, const string& menu);
+	bool IntOption(const string& option, int *var, int min, int max, int stepSize = 1);
+	bool FloatOption(const string& option, float *var, float min, float max, float stepSize = 0.1f);
+	bool BoolOption(const string& option, bool *var);
+
+	void DrawMenu();
+	void UpdateInput();
+
+	void SetTitleTextColour(const RGBA& titleTextClr);
+	void SetTitleRectColour(const RGBA& titleRectClr);
+	void SetSelectionColour(const RGBA& selectionClr);
+	void SetOptionsColour(const RGBA& optionsClr);
+	void SetOptionsRectColour(const RGBA& optionsRectClr);
 private:
-	static enum MenuSounds
+	enum MenuSounds
 	{
 		BACK,
 		SELECT,
@@ -36,8 +55,34 @@ private:
 		CONTINUE
 	};
 
-	static void _DrawText(string text, int font, float x, float y, float scaleX, float scaleY, RGBA colour, bool centered);
-	static void DrawRect(float x, float y, float width, float height, RGBA colour);
-	static void DrawSprite(char* Streamedtexture, char* textureName, float x, float y, float width, float height, float rotation, RGBA colour);
-	static void PlayMenuSound(const MenuSounds sound);
+	const float menuX = 0.2f;
+
+	RGBA titleTextColour;
+	RGBA titleRectColour;
+	RGBA selectionColour;
+	RGBA optionColour;
+	RGBA optionsRectColour;
+
+	int optionCount;
+	int currentOption;
+	int menuLevel;
+
+	DWORD tickDelay;
+
+	bool optionPressed;
+	bool leftPressed;
+	bool rightPressed;
+	bool downPressed;
+	bool upPressed;
+
+	string currentMenu = "";
+
+	std::array<string, MAX_MENU_COUNT> menuList;
+	std::array<int, MAX_OPTION_COUNT> optionList;
+	
+
+	void _DrawText(string text, int font, float x, float y, float scaleX, float scaleY, RGBA colour, bool centered) const;
+	void DrawRect(float x, float y, float width, float height, RGBA colour) const;
+	void DrawSprite(char* Streamedtexture, char* textureName, float x, float y, float width, float height, float rotation, RGBA colour) const;
+	void PlayMenuSound(const MenuSounds sound) const;
 };
--- GTA5MenuVC++/utils.h
+++ GTA5MenuVC++/utils.h
@@ -7,3 +7,7 @@ using std::string;
 void showSubtitle(const string& text, const int duration);
 void showNotification(const string& text);
 char* stringToCharPtr(string str);
+
+Vector3 vector3Add(const Vector3& vec1, const Vector3& vec2);
+
+Vector3 vector3Multiply(Vector3 vector, float val);
